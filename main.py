b06cc97c38e3b748e541d2a80866115f64b67706
gaoqian310 <68983944+gaoqian310@users.noreply.github.com>
2021-12-01 15:45:23 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ df.head()
 df.info()
 
 
+# ## Quesiton 1: What are the age demographics of customers?
+
 # In[4]:
 
 
@@ -37,6 +39,9 @@ sns.set(rc={'figure.figsize':(25,20)})
 sns.countplot(x = df['Present Age'],data = df,palette = ['#338999'])
 
 
+# ## Question 2: What is the contribution of each individual compaigns in the total response.
+# the response to previous compaigns are stored in "AcceptedCmp1-5" and the most recently launched campaign is in "Response". "TotalPurchaseThroughCampaign" is created to visualize the contribution of individual campaigns.
+
 # In[6]:
 
 
@@ -45,7 +50,9 @@ df['TotalPurchaseThroughCampaign'].head()
 sns.heatmap(data = df[['AcceptedCmp1','AcceptedCmp2','AcceptedCmp3','AcceptedCmp4','AcceptedCmp5','Response','TotalPurchaseThroughCampaign']].corr(),annot = True)
 
 
-# In[7]:
+# ## Calculating percentage of purchases through each campaign
+
+# In[23]:
 
 
 #calculating percentage of purchases through each campaign
@@ -86,7 +93,9 @@ for i in df['Response']:
 print((count/2240)*100) 
 
 
-# In[8]:
+# ## Question 3: What are the education demographics of customers? 
+
+# In[25]:
 
 
 plt.figure(figsize=(10,7))
@@ -96,11 +105,27 @@ for i in range(len(l)):
     l[i] = l[i]+"("+str(a[i]*100/len(data["Education"])) + "%)"
 plt.pie(a,labels=l, radius=2)
 plt.show()
+
+
+# ## Question 4: What the relation between the education level and number of kids?
+
+# In[27]:
+
+
 #relation between education and number of kids
 sns.displot(x="Education",y="Kidhome",data=data)
+
+
+# ## What's the relation between education level and response to the campaign?
+
+# In[28]:
+
+
 #relation between education and response to the campaign
 sns.displot(x="Education",y="Response",data=data)
 
+
+# ## What's the corrolation we can discover from the heatmaps?
 
 # In[9]:
 
@@ -110,6 +135,9 @@ sns.set(rc={'figure.figsize':(25,20)})
 sns.heatmap(data = df.corr(),annot=True)
 
 
+# We can see the pattern that edible itesm such as wine, fruit, meet an fish are often brought together and often brouhgt from the store.
+# And if a customer have a kid at home, he tend to order online to avoid taking kid out to shop for groceries.
+
 # In[13]:
 
 
@@ -118,11 +146,7 @@ sns.set(rc={'figure.figsize':(5,5)})
 sns.heatmap(df[['Income','NumStorePurchases']].corr(),annot=True)
 
 
-# In[ ]:
-
-
-
-
+# We can see with the income increases, the customers prefer to by their products in store rather than from a website.
 
 # In[ ]:
 
